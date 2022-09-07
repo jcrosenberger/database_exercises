9bf8681347b86bbf16bea6c0c03db9e525ccaed0
Chris Rosenberger <jcrosenberger@pm.me>
2022-09-07 14:48:55 -0500

    Doing classwork for the SQL tables exercise(s)

---
+++ tables_exercises.sql
@@ -0,0 +1,21 @@
+# 1  Open MySQL Workbench and login to the database server
+# Done. 
+
+# 2  Save your work in a file named tables_exercises.sql
+# Created. 
+
+# 3  Use the employees database. Write the SQL code necessary to do this.
+USE employees;
+
+# 4  List all the tables in the database. Write the SQL code necessary to accomplish this.
+USE employees;
+SHOW FULL TABLES;
+
+# 5  Explore the employees table. What different data types are present on this table?
+
+
+# 6  Which table(s) do you think contain a numeric type column? (Write this question and your answer in a comment)
+# 7  Which table(s) do you think contain a string type column? (Write this question and your answer in a comment)
+# 8  Which table(s) do you think contain a date type column? (Write this question and your answer in a comment)
+# 9  What is the relationship between the employees and the departments tables? (Write this question and your answer in a comment)
+# 10 Show the SQL that created the dept_manager table. Write the SQL it takes to show this as your exercise solution.
